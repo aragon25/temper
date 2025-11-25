fe68af856aeff812f5901f1edaad21ada6298c9a
aragon25 <aragon25.01@web.de>
2025-11-25 03:59:30 +0100

    usage fix

--- src/temper-daemon.sh
+++ src/temper-daemon.sh
@@ -164,7 +164,7 @@ function cmd_print_version() {
 }
 
 function cmd_print_help() {
-  echo "Usage: $(basename ""$0"") [OPTION]"
+  echo "Usage: $SCRIPTNAME [OPTION]"
   echo "$SCRIPT_TITLE v$SCRIPT_VERSION"
   echo " "
   echo "Get Temperatures from temper devices every 10sec. and"
